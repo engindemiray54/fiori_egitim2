009d577198749fe373bbc5fcd373173a5e68a1c9
engindemiray54 <engin.demiray@tirsan.com>
2022-12-09 09:39:26 +0300

    6.ders

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,11 +1,15 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], function (Controller) {
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "sap/m/MessageBox"
+], function (Controller, MessageToast, MessageBox) {
    "use strict";
    return Controller.extend("sap.ui.demo.walkthrough.controller.App", {
       onShowHello : function () {
          // show a native JavaScript alert
-         alert("Hello World");
+         // alert("Hello World");
+         // MessageToast.show("Hello World");
+         MessageBox.confirm("Hello Engin");
       }
    });
 });
